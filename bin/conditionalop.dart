7c7d3b6a4f3cffbc124e03cf194dacc7f6bb6269
Alwin VJ <alwinvj5@gmail.com>
2022-11-24 14:47:13 +0530

    Second commit from Luminar

---
+++ bin/conditionalop.dart
@@ -0,0 +1,6 @@
+// This program shows the working of conditional operator
+void main(){
+  var age = 26;
+  var eligibility = age > 18 ? 'Eligible to vote' : 'Not eligible to vote';
+  print(eligibility);
+}
