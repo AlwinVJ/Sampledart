689bdbd091669d5ed982a589811e4c32a73e79ab
Alwin VJ <alwinvj5@gmail.com>
2022-11-25 15:11:24 +0530

    First commit on Friday

---
+++ bin/samplecontrol.dart
@@ -0,0 +1,63 @@
+// This functions shows the functioning of different control statements
+import 'dart:io';
+
+void main(){
+ // simple if 
+ var age = 18;
+ if(age >= 18){
+  print("Eligible to vote");
+ }
+ print('Thank You');
+ // if else statement
+
+ if(age>=18){
+  print("Eligible to vote");
+ }else{
+  print("Not eligible to vote");
+ }
+ //The following code shows greatest of 2 numbers
+print("Enter two numbers to find the largest among them");
+int a = int.parse(stdin.readLineSync()!);
+int b = int.parse(stdin.readLineSync()!);
+if (a>b){
+  print("The greatest number is $a");
+}else{
+  print("The greatest number is $b");
+}
+// Nested if else statement
+var uname = "Alwin V J";
+var password = "qwerty";
+
+if(uname == "Alwin V J" && password == "qwerty"){
+  if(password.length>=8){
+    print("Login successful");
+  }else{
+    print("Short Password");
+  }
+}else{
+  print("Login failure");
+}
+int otp = 12345;
+if(uname == "Alwin V J" && password == "qwerty"){
+  if(otp == 12345){
+    print("Login succesful, OTP verified");
+  }else{
+    print("Wrong OTP");
+  }
+}else{
+  print("Login failed");
+}
+// Code to check whether passed (with or without distinction) or failed
+print("Enter your marks to know the result");
+int marks = int.parse(stdin.readLineSync()!);
+if(marks >= 40){
+  stdout.write("Passed");
+  if (marks>=80){
+    print(" With Distinction");
+  }else{
+    print(" Without Distinction");
+  }
+}else{
+  print("Failed");
+}
+}
