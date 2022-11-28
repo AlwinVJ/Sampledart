245510eb23b29d06d41fc286532b11a45d153fe3
Alwin VJ <alwinvj5@gmail.com>
2022-11-28 14:43:12 +0530

    First commit on 28th November

---
+++ bin/iteration.dart
@@ -0,0 +1,7 @@
+// This program shows the funtionality of iteration statements and their syntax
+void main(){
+  // for loop syntax and functionality
+  for(int count = 1;count <=5; count++){
+    print("Hello");
+  }
+}
