2dcce16699de0934b49c14c01fab1dc8da8d25b2
Alwin VJ <alwinvj5@gmail.com>
2022-11-28 16:04:06 +0530

    Second commit on 28th November

---
+++ bin/collection/listcreation.dart
@@ -0,0 +1,26 @@
+// This code shows the creation of list with values directly assigned to the list
+// There are several ways to create and assign values to list
+void main(){
+  List<dynamic> datas = ["Alwin",26,"Alan",26,"Sidarth",25,"Arju",25,"Basil",26,"Aida",24,"Johny",58,"Elsy",53];
+  print(datas);
+  List<int> numbers = [1,2,3,4,5];
+  print(numbers);
+  // demonstration of uses of iteration statements in collection
+  int sum = 0;
+  for(int index = 0; index<numbers.length;index++){
+    sum = numbers[index] + sum;
+  }
+  print("The sum is $sum");
+  // The code below prints the count of even and odd numbers seperately from a given list
+  int ce = 0, cd = 0;
+  for(int i = 0; i<numbers.length; i++ ){
+    if(numbers[i]%2 == 0){
+      ce++;
+    }
+    else{
+      cd++;
+    }
+  }
+  print("The count of odd numbers in the given list is $cd");
+  print("The count of even numbers in the given list is $ce");
+  }
--- bin/iteration.dart
+++ bin/iteration.dart
@@ -1,7 +1,72 @@
 // This program shows the funtionality of iteration statements and their syntax
+import 'dart:io';
+
 void main(){
   // for loop syntax and functionality
   for(int count = 1;count <=5; count++){
     print("Hello");
   }
+  //This code prints the numbers from 1 to 10)
+  stdout.write("The numbers from one to ten are :");
+  for(int count = 1; count <=10; count++){
+    stdout.write("$count ,");
+    }
+  // This code print the odd numbers in between one and ten
+  print("The odd numbers between one and ten are given below");
+  for (int count = 1;count<=10;count++){
+    if(count%2!=0){
+      print(count);
+    }
+  }
+  // This code prints the even numbers in between one and ten
+  print("The even numbers between one and ten are given below");
+  for (int count = 1;count<=10;count++){
+    if(count%2 == 0){
+      print(count);
+    }
+  } 
+  // This code prints the sum of first 10 natural numbers
+  int sum = 0;
+  stdout.write("The sum of first ten natural numbers is ");
+  for(int count = 1; count<= 10; count++){
+    sum = sum + count;
+  }
+  print(sum);
+  // This code finds the sum of even and odd numbers in between 1 and 10
+  int se = 0, so = 0;
+  for(int count = 1; count<=10;count++){
+    if(count%2 == 0){
+      se = se+count;
+    }
+    else{
+      so = so + count;
+    }
+  }
+  print("The sum of even numbers between one and ten is $se");
+   print("The sum of odd numbers between one and ten is $so");
+   
+   // This code prints the multiplication table of a number from one to ten
+   print('Enter a number to gets it standard multiplication table');
+   // Syntax to intake or input a number from the user
+   int num = int.parse(stdin.readLineSync()!);
+   for(int count = 1;count <= 10; count++){
+    int mult = 0;
+    mult = count*num;
+    print("$num*$count = $mult");
+   }
+   // This code shows the syntax and funtioning of while loop
+    print("The numbers from 1 to 10 are given below");
+    int count = 0;
+    while(count <= 9){
+      count++;
+      print(count);
+    }
+// while loop is also known as entry controlled loop
+// The code below shows the syntax and funtioning of do-while loop
+int d = 30;
+do{
+  print(d);
+  d--;}while(d>=20);// A do while loop end with the while/condition checking statement 
+ //with a semi-colon(;) and will always execute atleast once and is also known as exit 
+ // control loop statement
 }
