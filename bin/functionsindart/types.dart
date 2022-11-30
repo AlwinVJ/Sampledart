e133e3fee81c7967d522bd136349889f95624d0b
Alwin VJ <alwinvj5@gmail.com>
2022-11-30 14:49:18 +0530

    First commit on 30th Nov

--- bin/functionsindart/types.dart
+++ bin/functionsindart/types.dart
@@ -9,7 +9,13 @@ funtion5('Flutter', rate: 25000, hours:3);// calling optional named function ass
 // other than the a default to the 'hour' variable
 funtion5('Flutter', rate: 25000);// calling optional named function with default 
 //parameters
-
+show();
+print(shows());
+function6();
+print(function6());// A way of calling a default function with a return type
+//Another way of calling a default function with a return type.
+var c = function6();
+print(c);
 }
 // The first function is called default function, an example is given below
 void function1(){
@@ -39,3 +45,21 @@ void function4(String name, {int? age, required double cgpa}){
 void funtion5(String course, {int? rate, double hours = 2}){
   print("Course = $course, rate = ₹$rate and hours = $hours hrs");
 }
+
+// Lambda function or Arrow function
+// Used only if there is a single executable statement
+// The code below shows the syntax of lambda function
+void show() => print("Hello all.");
+
+// The code below shows a lambda function with a return type
+
+String shows() => 'Hello';
+// Functions with return type
+// Functions with return type must be provided with a jump statement ie.., return statement
+// Default functions with return type
+
+String function6(){
+  print('Hello');
+  return 'Hai';
+}
+
