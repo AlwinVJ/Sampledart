d007951d8e910c12ab1896694873c2d124d7d015
Alwin VJ <alwinvj5@gmail.com>
2022-12-05 16:07:07 +0530

    Fourth commit updated on 5th Dec

---
+++ bin/superkey.dart
@@ -0,0 +1,23 @@
+//This program shows the use of "super" keyword
+
+import 'dart:html';
+import 'dart:io';
+
+class Parent{
+  void parents(){
+    print("Enter Father's name");
+    String fname = stdin.readLineSync()!;
+    print("Enter Mother's name");
+    String mname = stdin.readLineSync()!;
+  }
+}
+class Child extends Parent{
+  void child(){
+  print("Enter Child's name");
+  String cname = stdin.readLineSync()!;
+  Parent p = Parent();
+  p.parents();
+
+  }
+  
+}
