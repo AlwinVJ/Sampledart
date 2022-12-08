2c6cc908941d13dd770b98bcb6caeac4544cb03a
Alwin VJ <alwinvj5@gmail.com>
2022-12-08 15:33:25 +0530

    Third commit on 8th Dec

--- bin/oops/interface1.dart
+++ bin/oops/interface1.dart
@@ -1,2 +1,40 @@
 /// This program demonstrates interface
 
+class Father{
+  fatherdetails(String name, String job){}
+}
+abstract class Mother{
+  void motherdetails();
+}
+class Child1 implements Father, Mother {
+  @override
+  fatherdetails(String name, String job) {
+    print("Father's name is $name");
+    print("Father's job is $job");}
+
+  @override
+  void motherdetails() {
+    print("Mother's name is Elsy and Mother's job is homemaker");
+  }
+}
+class Child2 implements Father, Mother{
+   @override
+  fatherdetails(String name, String job) {
+    print("Father's name is $name");
+    print("Father's job is $job");}
+
+  @override
+  void motherdetails() {
+    print("Mother's name is Elsy and Mother's job is homemaker");
+  }
+  
+
+}
+void main(List<String> args) {
+  var obj = Child1();
+  obj.fatherdetails("Johny", "LoadWorker");
+  obj.motherdetails();
+  var obj1 = Child2();
+  obj1.fatherdetails("Johny","Loading and Unloading");
+  obj1.motherdetails();
+}
