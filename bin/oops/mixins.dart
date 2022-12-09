b21589eef72cd0c940476b423caee561d1a72196
Alwin VJ <alwinvj5@gmail.com>
2022-12-09 14:50:49 +0530

    First commit on 9th December

---
+++ bin/oops/mixins.dart
@@ -0,0 +1,32 @@
+// This program shows the funcionality of mixins
+// It can be used for multiple inheritance but without complete abstraction more like
+// a class
+// Keyword used for mixins is mixin and inherit mixins using the keyword with
+
+mixin A {
+  String name = 'Alan';
+  int age = 26;
+
+  void display(){
+    print("My name is $name and my age is $age");
+  }
+}
+ mixin B {
+  String input = 'Without a body';
+
+  void display1(){
+    print("This is without body");
+  }
+ }
+ 
+ class C with A, B {
+  @override
+  void display(){
+    print("This is overridden");
+  }
+ }
+ void main(){
+  var obj = C();
+  obj.display();
+  obj.display1();
+ }
